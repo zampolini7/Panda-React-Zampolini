eb1b2cd013634b114cafe2fb002a8d8e5a8b02d5
Nico Zampolini <zampolini7@gmail.com>
2021-06-06 21:22:44 -0300

    cambios zamp

--- src/index.js
+++ src/index.js
@@ -5,27 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { CartProvider } from './context/cartContext'
 
-const itemExample = [{id: 3,
-  title: "WineBox",
-  price: 3300,
-  description: "Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.\n\nIn quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.",
-  imageId: "images1/winebox1.jpeg",
-  stock: 1
-},
-{id: 3,
-  title: "WineBox",
-  price: 3300,
-  description: "Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.\n\nIn quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.",
-  imageId: "images1/winebox1.jpeg",
-  stock: 1
-}
 
-]
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <CartProvider value= {[itemExample]}>
+    <CartProvider value= {[]}>
       <App />
     </CartProvider>
 
